8acdf874c0c053dfc592cb0e625317e64d1e9e11
cara ottmar <cara.ottmar@gmail.com>
2019-05-14 10:23:58 -0700

    saving after css works after adding css modules

--- wcag-components/src/App.js
+++ wcag-components/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Button from './Button.js';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    document.title = 'Web Accessible React Components';
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <p>Hello?</p>
+        <Button />
+      </div>
+    )
+  }
+
 }
 
 export default App;
